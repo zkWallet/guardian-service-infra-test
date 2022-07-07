8589f52ddeefcd581fb7ea2092429ecd74a9a705
iam-dev <elky.bachtiar@duo.nl>
2022-07-07 13:19:04 +0200

    jwt expiresIn 1h

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
